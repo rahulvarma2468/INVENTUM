a16401a9e13e35e0b4e40750632ec4b446b27e7a
Rahul <rahulvarma4092@gmail.com>
2025-12-16 10:48:27 +0530

    minor changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -37,7 +37,7 @@ function Contact() {
     {
       name: 'Rahul varma Mudunuru',
       phone: '8125013863',
-      role: 'Fest Convenor & Treasurer',
+      role: 'Fest Convenor',
       img: '/images/team/rahul.jpg'
     },
     {
